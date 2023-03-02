0b21170f3c21fce9459ce0a6dafc33a62bc8f9d4
Igor <zhigalovzenit112233@gmail.com>
2023-03-02 21:29:08 +0300

    load packs

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { Route, Router, useHistory } from 'react-router-dom';
+import Header from './components/Header';
+// import reactLogo from './assets/react.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const history = useHistory();
+  const handleClick = () => {
+    // history.push('/header');
+    // history.replace('http://127.0.0.1:5500/');
+    // http://127.0.0.1:5500/
+    // a
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <h1>Привет мир</h1>
+      <div className="d-flex justify-content-center">
+        {/* <a href=""></a> */}
+        {/* <button className="btn btn-primary" onClick={handleClick}> */}
+        Переход к хедеру
+        <a href="gis-diplom\src\pages\map.html">sa</a>
+        {/* </button> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+
+      <Route path={'/header'} component={() => <Header />} exact />
+    </>
+  );
 }
 
-export default App
+export default App;
