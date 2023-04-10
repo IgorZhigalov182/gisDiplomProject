c6c5dbb0d724b64787b9b5dfc1934c82095d6ce8
Igor <zhigalovzenit112233@gmail.com>
2023-04-10 23:34:14 +0300

    some add in exercise9

--- src/layouts/exercise9/exercise9.jsx
+++ src/layouts/exercise9/exercise9.jsx
@@ -5,6 +5,12 @@ import exe9_2 from '../../../public/img/exe9_2.png';
 import exe9_3 from '../../../public/img/exe9_3.png';
 import exe9_4 from '../../../public/img/exe9_4.png';
 import exe9_5 from '../../../public/img/exe9_5.png';
+import exe9_6 from '../../../public/img/exe9_6.png';
+import exe9_7 from '../../../public/img/exe9_7.png';
+import exe9_8 from '../../../public/img/exe9_8.png';
+import exe9_9 from '../../../public/img/exe9_9.png';
+import exe9_10 from '../../../public/img/exe9_10.png';
+import exe9_11 from '../../../public/img/exe9_11.png';
 
 const Exercise9 = ({ count }) => {
   return (
@@ -51,8 +57,42 @@ const Exercise9 = ({ count }) => {
       {count === 3 && (
         <>
           <h5>После чего слайд запечатлится и будет выведен в список.</h5>
-
           <img src={exe9_5} className="exe9_5" />
+          <p>
+            Попробуйте самостоятельно повторить следующие шаги пока город Санкт-Петербург не будет
+            максимально близко: приблизиться и сделать слайд
+          </p>
+        </>
+      )}
+
+      {count === 4 && (
+        <>
+          <h5>Должно было получиться около 8-9 слайдов</h5>
+          <img src={exe9_6} className="exe9_6" />
+          <p>
+            Внизу должны появиться слайды по которым можно переключаться и следить за плавным
+            приближением к Санкт-Петербургу
+          </p>
+          <img src={exe9_7} className="exe9_7" />
+        </>
+      )}
+
+      {count === 5 && (
+        <>
+          <h5>Для дальнейшего выполнения задания давайте выберем последний слайд:</h5>
+          <img src={exe9_8} className="exe9_8" />
+          <p>Чтобы находить объекты на карте можно воспользоваться поиском:</p>
+          <img src={exe9_9} className="exe9_9" />
+        </>
+      )}
+
+      {count === 6 && (
+        <>
+          <p>После чего выбираем самую первую запись из списка</p>
+          <img src={exe9_10} className="exe9_10" />
+          <p>Чтобы находить объекты на карте можно воспользоваться поиском:</p>
+          <img src={exe9_11} className="exe9_11" />
+          <p>Карта автоматически...</p>
         </>
       )}
     </>
